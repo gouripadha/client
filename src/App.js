2b4826ee7de22855d1dbc2b12d1f09e2dbf9e846
Dharmesh Sharma <onlinedk2k@gmail.com>
2022-11-25 23:40:37 +0530

    fasdf

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from "react";
 import Home from "./pages/Home";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <Home />
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
