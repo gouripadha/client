d76fd45c2a2a0ca2bd76a69eb9a3feec647591c3
Gouri Padha <gouripadha@gmail.com>
2022-12-01 14:06:58 +0530

    bug is fixed

--- src/pages/User/UserUpdate.js
+++ src/pages/User/UserUpdate.js
@@ -8,13 +8,13 @@ import Axios from "axios";
 import "./UserUpdate.css";
 
 function UserUpdate() {
-  const [UserList, setUserList] = useState([]);
+  const [User, setUser] = useState([]);
   const [newChildName, setnewChildName] = useState("");
   const [newParentName, setnewParentName] = useState("");
   const [newMedicalHistory, setnewMedicalHistory] = useState("");
   const [newYear, setnewYear] = useState(0);
-
   const [newContact, setnewContact] = useState(0);
+
   const navigate = useNavigate();
   useEffect(() => {
     if (localStorage.getItem("email") == null) {
@@ -27,13 +27,28 @@ function UserUpdate() {
       `http://localhost:3001/profile/${localStorage.getItem("email")}`
     );
     const data = await response.json();
-    setUserList(data);
+    console.log(data[0]);
+    setUser(data[0]);
+    setnewChildName(data[0].child_name);
+    setnewParentName(data[0].parent_name);
+    setnewMedicalHistory(data[0].medical_history);
+    setnewYear(data[0].year);
+    setnewContact(data[0].contact);
   };
+
   useEffect(() => {
     fetchStudents();
   }, []);
-
+  useEffect(() => {
+    console.log(User);
+  }, []);
   const updateUser = (email) => {
+    console.log(newChildName);
+    console.log(newParentName);
+    console.log(newMedicalHistory);
+    console.log(newYear);
+    console.log(newContact);
+
     Axios.put("http://localhost:3001/updateuser", {
       child_name: newChildName,
       parent_name: newParentName,
@@ -43,21 +58,7 @@ function UserUpdate() {
       email: email,
     }).then((response) => {
       console.log(response);
-      setUserList(
-        UserList.map((val) => {
-          return val.email == email
-            ? {
-                child_name: newChildName,
-                parent_name: newParentName,
-                medical_history: newMedicalHistory,
-                year: newYear,
-
-                contact: newContact,
-                email: email,
-              }
-            : val;
-        })
-      );
+      navigate("/user/profile");
     });
   };
 
@@ -82,77 +83,72 @@ function UserUpdate() {
         <span className="Children">Profile</span>
       </Link>
       <div className="spacer" />
-
-      {UserList.map((val, key) => {
-        return (
-          <div className="individual">
-            <span className="ActivityIdXXX">
-              Child Name:{" "}
-              <input
-                type="text"
-                defaultValue={val.child_name}
-                onChange={(event) => {
-                  setnewChildName(event.target.value);
-                }}
-                required
-              />
-            </span>
-            <span className="ActivityIdXXX">
-              Parent Name:{" "}
-              <input
-                className="Parent"
-                type="text"
-                defaultValue={val.parent_name}
-                onChange={(event) => {
-                  setnewParentName(event.target.value);
-                }}
-                required
-              />
-            </span>
-            <span className="ActivityIdXXX">
-              Medical History:{" "}
-              <input
-                type="text"
-                defaultValue={val.medical_history}
-                onChange={(event) => {
-                  setnewMedicalHistory(event.target.value);
-                }}
-                required
-              />
-            </span>
-            <span className="ActivityIdXXX">
-              Year:{" "}
-              <input
-                type="number"
-                defaultValue={val.year}
-                onChange={(event) => {
-                  setnewYear(event.target.value);
-                }}
-                required
-              />
-            </span>
-            <span className="ActivityIdXXX">
-              Contact:{" "}
-              <input
-                type="number"
-                defaultValue={val.contact}
-                onChange={(event) => {
-                  setnewContact(event.target.value);
-                }}
-                required
-              />
-            </span>
-            <button
-              className="Addbutton"
-              onClick={() => {
-                updateUser(val.email);
-              }}
-            >
-              Update
-            </button>
-          </div>
-        );
-      })}
+      <div className="individual">
+        <span className="ActivityIdXXX">
+          Child Name:{" "}
+          <input
+            type="text"
+            defaultValue={User.child_name}
+            onChange={(event) => {
+              setnewChildName(event.target.value);
+            }}
+            required
+          />
+        </span>
+        <span className="ActivityIdXXX">
+          Parent Name:{" "}
+          <input
+            className="Parent"
+            type="text"
+            defaultValue={User.parent_name}
+            onChange={(event) => {
+              setnewParentName(event.target.value);
+            }}
+            required
+          />
+        </span>
+        <span className="ActivityIdXXX">
+          Medical History:{" "}
+          <input
+            type="text"
+            defaultValue={User.medical_history}
+            onChange={(event) => {
+              setnewMedicalHistory(event.target.value);
+            }}
+            required
+          />
+        </span>
+        <span className="ActivityIdXXX">
+          Year:{" "}
+          <input
+            type="number"
+            defaultValue={User.year}
+            onChange={(event) => {
+              setnewYear(event.target.value);
+            }}
+            required
+          />
+        </span>
+        <span className="ActivityIdXXX">
+          Contact:{" "}
+          <input
+            type="number"
+            defaultValue={User.contact}
+            onChange={(event) => {
+              setnewContact(event.target.value);
+            }}
+            required
+          />
+        </span>
+        <button
+          className="Addbutton"
+          onClick={() => {
+            updateUser(User.email);
+          }}
+        >
+          Update
+        </button>
+      </div>
     </div>
   );
 }
