85a56beaaf6c71526aec0db2860dd2ad2adefedc
Dharmesh Sharma <onlinedk2k@gmail.com>
2022-12-01 14:09:27 +0530

    bb

--- src/pages/AdminLogin.js
+++ src/pages/AdminLogin.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "./AdminLogin.css";
 // import { UserContext } from "./../UserContext";
-import sgn from "../images/UserLogin_image4.png";
+import sgn from "../images/priscilla-du-preez--mCXEsLd2sU-unsplash.jpg";
 import { useState, useContext } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import Axios from "axios";
